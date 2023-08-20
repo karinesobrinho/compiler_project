1ebffe513ee16ea8aa1b9a20cac55fb5805f5a22
juliaocosta <julia_oc@outlook.com>
2023-08-20 01:22:39 -0300

    adicionando arquivos de teste.isi

--- src/br/com/edu/ufabc/isilanguage/main/MainClass.java
+++ src/br/com/edu/ufabc/isilanguage/main/MainClass.java
@@ -1,8 +1,12 @@
 package br.com.edu.ufabc.isilanguage.main;
 
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+
+import br.com.edu.ufabc.isilanguage.exceptions.IsiSemanticException;
 import br.com.edu.ufabc.isilanguage.parser.langLexer;
 import br.com.edu.ufabc.isilanguage.parser.langParser;
-import br.com.edu.ufabc.isilanguage.exceptions.IsiSemanticException;
+
 
 import org.antlr.v4.runtime.*;
 public class MainClass {
@@ -11,20 +15,21 @@ public class MainClass {
             langLexer lexer;
             langParser parser;
 
-            lexer = new langLexer(CharStreams.fromFileName("input.isi"));
+            lexer = new langLexer(CharStreams.fromFileName("GitHub_compiler_project/src/Resources/teste1.isi"));
             CommonTokenStream tokenStream = new CommonTokenStream(lexer);
 
             parser = new langParser(tokenStream);
+            parser.prog();
             System.out.println("Compilation Successful!");
 
             parser.exibeComandos();
             parser.generateCode();
+            /*parser.Warnings();*/
 
         } catch (IsiSemanticException ex) {
             System.err.println("Semantic error - " + ex.getMessage());
         } catch (Exception ex) {
             System.err.println("ERROR - " + ex.getMessage());
-            System.out.println("SAMI TESTES");
         }
     }
 }
