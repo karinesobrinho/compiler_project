6b65bd52c303a8c6600813025d429a84037fd118
Karol Medeiros <90484249+Woonson@users.noreply.github.com>
2023-08-20 13:26:38 -0300

    arrumar maim

--- src/br/com/edu/ufabc/isilanguage/main/MainClass.java
+++ src/br/com/edu/ufabc/isilanguage/main/MainClass.java
@@ -8,27 +8,30 @@ import br.com.edu.ufabc.isilanguage.parser.langLexer;
 import br.com.edu.ufabc.isilanguage.parser.langParser;
 
 public class MainClass {
-    public static void main(String[] args){
-        try{
-            langLexer lexer;
-            langParser parser;
+	public static void main(String[] args) {
+		try {
+			langLexer lexer;
+			langParser parser;
 
-            //lexer = new langLexer(CharStreams.fromFileName("src\\Resources\\teste2.isi"));
-            lexer = new langLexer(CharStreams.fromFileName("src/Resources/teste2.isi"));
+			lexer = new langLexer(CharStreams.fromFileName("src/Resources/teste2.isi"));
             CommonTokenStream tokenStream = new CommonTokenStream(lexer);
+			
+			parser = new langParser(tokenStream);			
+			parser.prog();
+			System.out.println("Compilation Successful");
+			
+			parser.exibeComandos();                      
+			parser.generateCode();
+            parser.Warning();
+		}
+		catch(IsiSemanticException ex) {
+			System.err.println("Semantic error - "+ex.getMessage());
+		}
+		catch(Exception ex) {
+			ex.printStackTrace();
+			System.err.println("ERROR "+ex.getMessage());
+		}
+		
+	}
 
-            parser = new langParser(tokenStream);
-            parser.prog();
-            System.out.println("Compilation Successful!");
-
-            parser.exibeComandos();
-            parser.generateCode();
-            /*parser.Warnings();*/
-
-        } catch (IsiSemanticException ex) {
-            System.err.println("Erro semântico- " + ex.getMessage());
-        } catch (Exception ex) {
-            System.err.println("Erro - " + ex.getMessage());
-        }
-    }
 }
